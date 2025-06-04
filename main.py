f4825e29d3e5e3a663bd27d14b906e0f98553baf
AidenBow <aiden.bowcott@gmail.com>
2025-06-04 13:04:08 -0700

    added asteroid field and collison check

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
 import pygame
 from constants import *
-from player import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     print("Starting Asteroids!")
@@ -15,7 +14,16 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
+    
 
     while True:
         for event in pygame.event.get():
@@ -23,8 +31,13 @@ def main():
                 return
 
         screen.fill(color="black")
-        player.update(dt)
-        player.draw(screen)
+        updatable.update(dt)
+        for item in drawable:
+            item.draw(screen)
+        for asteroid in asteroids:
+            if player.collision_check(asteroid):
+                print("Game over!")
+                return
         pygame.display.flip()
         dt = clock.tick(60)/1000
 
